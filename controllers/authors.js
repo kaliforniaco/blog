55eecc20a883d1e10dfba2e6f6dad3bee571d423
David Epps <depps@Davids-MacBook-Air.local>
2018-07-16 15:12:37 -0600

    building second model lab 1

--- controllers/authors.js
+++ controllers/authors.js
@@ -1,51 +1,66 @@
 const express = require('express');
-const router = express.Router();
-const Author = require('../models/authors');
-//INDEX
-router.get('/', (req,res) => {
-	Author.find({}, (err, foundAuthors) => {
-	res.render('authors/index.ejs', {
-		authors: foundAuthors
-		});
-	});
+const router  = express.Router();
+const Author  = require('../models/authors');
+
+router.get('/', (req, res) => {
+  Author.find({}, (err, foundAuthors) => {
+      res.render('authors/index.ejs', {
+        authors: foundAuthors
+      });
+  });
+
 });
 
-//NEW
-router.get('/new', (req,res) => {  /// SHOW route HAS to be AFTER new, or you'll NEVER hit NEW
-	res.render('authors/news.ejs');
-})
-
-router.get('/:id', (req,res) => {
-	Author.findById(rec.params.id, (err, foundAuthor) =>{
-		res.render('authors/show.ejs', {
-			author: foundAuthor
-		});
-	});
-});
-//EDIT
-router.get('/:id/edit', (req,res) => {
-	Author.findById(req.params.id, (err, foundAuthor) => {
-		res.sender('authors/edit.ejs', {
-			author: foundAuthor
-		});
-	});
+router.get('/new', (req, res) => {
+  res.render('authors/new.ejs');
 });
 
-router.post('/', (req,res) => {
-	console.log(req.body)
-	Author.create(rec.body, (err, createdAuthor) => {
-		console.log(createdAuthor, 'this is the createdAuthor')
-		res.redirect('/authors');
-	});
-	res.send('server recieved request')
+
+router.get('/:id', (req, res) => {
+  Author.findById(req.params.id, (err, foundAuthor) => {
+    res.render('authors/show.ejs', {
+      author: foundAuthor
+    });
+  });
 });
 
-router.delete('/:id', (req,res) => {
-	Author.findByIdAndRemove(req.params.id, (err, deletedAuthor) => {
-		console.log(deletedAuthor, 'this is deletedAuthor');
-		res.redirect('/authors')
-	})
+router.get('/:id/edit', (req, res) => {
+
+  Author.findById(req.params.id, (err, foundAuthor) => {
+    res.render('authors/edit.ejs', {
+      author: foundAuthor
+    });
+  });
+
 });
+
+router.put('/:id', (req, res) => {
+  Author.findByIdAndUpdate(req.params.id, req.body, {new: true}, (err, updatedAuthor)=> {
+    console.log(updatedAuthor, ' this is updatedAuthor');
+    res.redirect('/authors');
+  });
+});
+
+
+router.post('/', (req, res) => {
+  console.log(req.body)
+  Author.create(req.body, (err, createdAuthor) => {
+    console.log(createdAuthor, ' this is the createdAuthor');
+    res.redirect('/authors');
+  });
+
+});
+
+
+router.delete('/:id', (req, res) => {
+
+  Author.findByIdAndRemove(req.params.id, (err, deletedAuthor) => {
+    console.log(deletedAuthor, ' this is deletedAuthor');
+    res.redirect('/authors')
+  })
+
+});
+
 
 
 module.exports = router;
--- server.js
+++ server.js
@@ -2,17 +2,20 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const methodOverride = require('method-override');
-
 require('./db/db');
+
 
 app.use(methodOverride('_method'));
 app.use(bodyParser.urlencoded({extended: false}));
 
-const authorsContoller = require('./controllers/authors.js');
+
+const authorsController = require('./controllers/authors.js');
 app.use('/authors', authorsController);
 
 
-
+app.get('/', (req, res) => {
+  res.render('index.ejs');
+});
 
 
 app.listen(3000, () => {
